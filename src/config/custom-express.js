21be7620ded8ad034ecf67cb2a0ed2f0882ca235
Wandberg de Lima <wandberg@gmail.com>
2022-11-20 10:56:00 -0300

    ok

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -3,9 +3,9 @@ require('marko/express');
 
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-
 const templates = require('../app/views/templates');
 
 app.use('/estatico', express.static('src/app/public'));
